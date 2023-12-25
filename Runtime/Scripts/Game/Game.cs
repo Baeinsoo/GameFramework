e73f7c2a8202843275c88a45c533e29cda8d8a7b
re5nardo <re5nardo@naver.com>
2023-12-25 19:40:32 +0900

    Game.Time 추가

--- Runtime/Scripts/Game/Game.cs
+++ Runtime/Scripts/Game/Game.cs
@@ -7,5 +7,14 @@ namespace GameFramework
     public class Game
     {
         public static IGame current { get; internal set; }
+
+        public class Time
+        {
+            public static long tick => Game.current.tickUpdater.tick;
+            public static double elapsedTime => Game.current.tickUpdater.elapsedTime;
+
+            /// <summary> Tick Delta Time </summary>
+            public static double deltaTime => Game.current.tickUpdater.tick == 0 ? 0 : Game.current.tickUpdater.interval;
+        }
     }
 }
