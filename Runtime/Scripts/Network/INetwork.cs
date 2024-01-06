358a909f6bc90cb77a57bdd51c6be94e75b711ab
re5nardo <re5nardo@naver.com>
2024-01-06 19:55:50 +0900

    IMessage, INetwork 추가

---
+++ Runtime/Scripts/Network/INetwork.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFramework
+{
+    public interface INetwork
+    {
+        event Action<IMessage> onMessage;
+
+        void Send(IMessage message, int targetId, bool reliable = true, bool instant = false);
+        void SendToAll(IMessage message, bool reliable = true, bool instant = false);
+        void SendToNear(IMessage message, Vector3 center, float radius, bool reliable = true, bool instant = false);
+    }
+}
