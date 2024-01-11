95267a9319f49b4b12e300d055d26000857f9751
re5nardo <re5nardo@naver.com>
2024-01-11 20:22:46 +0900

    AppDataStorage, SceneDataStorage 추가

---
+++ Runtime/Scripts/DataStorage/SceneDataStorage.cs
@@ -0,0 +1,37 @@
+using GameFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFramework
+{
+    public class SceneDataStorage : MonoSingleton<SceneDataStorage>
+    {
+        private DataStorage dataStorage;
+
+        protected override void OnAwaked()
+        {
+            dataStorage = gameObject.AddComponent<DataStorage>();
+        }
+
+        public static void Set<T>(T value)
+        {
+            instance.dataStorage.Set(value);
+        }
+
+        public static void Set<T>(string key, T value)
+        {
+            instance.dataStorage.Set(key, value);
+        }
+
+        public static bool Get<T>(out T value, bool delete = false)
+        {
+            return instance.dataStorage.Get(out value, delete);
+        }
+
+        public static bool Get<T>(string key, out T value, bool delete = false)
+        {
+            return instance.dataStorage.Get(key, out value, delete);
+        }
+    }
+}
