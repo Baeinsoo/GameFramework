3d8a2393752d28dc617c42882056b40c9ff97375
re5nardo <re5nardo@naver.com>
2024-05-23 23:54:30 +0900

    ScriptableObjectSingleton 추가

---
+++ Runtime/Scripts/Singleton/ScriptableObjectSingleton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GameFramework
+{
+    public class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObjectSingleton<T>
+    {
+        private static T _instance;
+        public static T instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var candidates = Resources.LoadAll<T>("");
+                    if (candidates == null || candidates.Length < 1)
+                    {
+                        throw new Exception($"Could not find any ScriptableObjectSingleton instance in the Resources.");
+                    }
+                    else if (candidates.Length > 1)
+                    {
+                        Debug.LogWarning($"Multiple instances of the ScriptableObjectSingleton found in the Resources.");
+                    }
+
+                    _instance = candidates[0];
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
