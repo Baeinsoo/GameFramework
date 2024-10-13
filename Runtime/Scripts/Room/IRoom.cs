9258ed46792cc21eef25c063dcec524d9d7c5cf6
re5nardo <re5nardo@naver.com>
2024-10-13 21:10:57 +0900

    IRoom 추가

---
+++ Runtime/Scripts/Room/IRoom.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFramework
+{
+    public interface IRoom : IInitializableAsync, IDeinitializableAsync
+    {
+        IGame game { get; }
+
+        void StartGame();
+        void StopGame();
+    }
+}
