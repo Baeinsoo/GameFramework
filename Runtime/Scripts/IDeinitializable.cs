d94e7a43ad70f59964f9f60a66420dd28e23044a
re5nardo <re5nardo@naver.com>
2024-10-13 19:48:45 +0900

    IDeinitializableAsync 추가

--- Runtime/Scripts/IDeinitializable.cs
+++ Runtime/Scripts/IDeinitializable.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 namespace GameFramework
 {
     public interface IDeinitializable
     {
         void Deinitialize();
+    }
+
+    public interface IDeinitializableAsync
+    {
+        Task DeinitializeAsync();
     }
 }
