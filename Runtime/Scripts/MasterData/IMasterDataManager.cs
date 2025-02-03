07137ca939116328bd0f21fc4fadfe2dc1ce574c
re5nardo <re5nardo@naver.com>
2025-02-03 22:02:53 +0900

    IMasterDataManager 인터페이스 수정

--- Runtime/Scripts/MasterData/IMasterDataManager.cs
+++ Runtime/Scripts/MasterData/IMasterDataManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace GameFramework
 {
     public interface IMasterDataManager
     {
+        Task LoadMasterData();
+
         void RegisterMasterData<T>(IEnumerable<T> collection) where T : IMasterData;
 
         T GetMasterData<T>(string code) where T : IMasterData;
