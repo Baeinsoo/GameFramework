867d01969de6f1053a82c2bd391f4ec860d544b7
re5nardo <re5nardo@naver.com>
2025-04-14 14:17:24 +0900

    불필요한 파일 제거

--- Runtime/Scripts/Dao/IDao.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace GameFramework
-{
-    public interface IDao<TEntity, TId> { }
-}
--- Runtime/Scripts/Repository/IRepository.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace GameFramework
-{
-    public interface IRepository<TEntity, TId> { }
-}
