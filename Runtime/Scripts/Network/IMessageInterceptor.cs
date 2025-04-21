c3d7a449ea776552a94a599f9912092dc2bb6592
re5nardo <re5nardo@naver.com>
2025-04-21 22:09:39 +0900

    IMessageInterceptorWithId 추가

--- Runtime/Scripts/Network/IMessageInterceptor.cs
+++ Runtime/Scripts/Network/IMessageInterceptor.cs
@@ -7,4 +7,11 @@ namespace GameFramework
         void OnAfterHandle<T>(T message) where T : IMessage;
         void OnError<T>(T message, string error) where T : IMessage;
     }
+
+    public interface IMessageInterceptorWithId
+    {
+        void OnBeforeHandle<T>(int id, T message) where T : IMessage;
+        void OnAfterHandle<T>(int id, T message) where T : IMessage;
+        void OnError<T>(int id, T message, string error) where T : IMessage;
+    }
 }
