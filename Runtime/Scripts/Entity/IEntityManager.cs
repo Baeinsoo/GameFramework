d75a54d947c0ac74200e2e810fb625a50194e590
re5nardo <re5nardo@naver.com>
2025-05-02 12:39:52 +0900

    TryGetEntity 추가

--- Runtime/Scripts/Entity/IEntityManager.cs
+++ Runtime/Scripts/Entity/IEntityManager.cs
@@ -13,6 +13,9 @@ namespace GameFramework
         IEntity GetEntity(string entityId);
         TEntity GetEntity<TEntity>(string entityId) where TEntity : IEntity;
 
+        bool TryGetEntity(string entityId, out IEntity entity);
+        bool TryGetEntity<TEntity>(string entityId, out TEntity entity) where TEntity : IEntity;
+
         IEnumerable<IEntity> GetEntities();
         IEnumerable<TEntity> GetEntities<TEntity>() where TEntity : IEntity;
 
