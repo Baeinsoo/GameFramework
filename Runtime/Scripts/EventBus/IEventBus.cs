4897c204d9d8bc0987d9e359249e1e962d333ba3
re5nardo <re5nardo@naver.com>
2025-06-08 15:18:04 +0900

    IEventBus 수정

--- Runtime/Scripts/EventBus/IEventBus.cs
+++ Runtime/Scripts/EventBus/IEventBus.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEngine;
 
 namespace GameFramework
 {
@@ -8,5 +7,6 @@ namespace GameFramework
         void Publish<T>(T eventData);
         void Subscribe<T>(Action<T> handler);
         void Unsubscribe<T>(Action<T> handler);
+        void Clear();
     }
 }
