a15dea3aab2c095b2b0febd15cdacde8a0f9b53f
re5nardo <re5nardo@naver.com>
2025-06-26 21:39:49 +0900

    기본 IsGrounded 구현

--- Runtime/Scripts/Extensions/Entity.Extensions.cs
+++ Runtime/Scripts/Extensions/Entity.Extensions.cs
@@ -1,3 +1,6 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
 
 namespace GameFramework
 {
@@ -11,6 +14,16 @@ namespace GameFramework
                 rotation = entity.rotation,
                 velocity = entity.velocity
             };
+        }
+
+        // TODO: 고도화 필요!
+        [Obsolete("임시 구현: 고도화 필요!")]
+        public static bool IsGrounded(this IEntity entity)
+        {
+            Vector3 checkPosition = entity.position + Vector3.down * 0.2f;
+            Collider[] colliders = Physics.OverlapSphere(checkPosition, 0.4f);
+
+            return colliders.Any(col => col.gameObject.name == "Plane");
         }
     }
 }
